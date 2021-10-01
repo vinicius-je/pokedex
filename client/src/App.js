bf38e1d75a195a820d46b6295485bc3ef4874257
Vinícius Jesus <vinicius.je99@gmail.com>
2021-10-01 16:55:02 -0300

    search pokemon by input value

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
+import { getPokemonData } from './pokemonApi'
 
 function App() {
+  const [pokemon, setPokemon] = useState([]); 
+  // get pokemon by search value in SerachBar   
+  async function getPokemonByInput(pokemon){
+      let pokemon_data = await getPokemonData(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+      setPokemon(pokemon_data)
+  }
+
+  useEffect(() => {
+      console.log(pokemon)
+  }, [pokemon])
   return (
     <div className="App">
-      <SearchBar></SearchBar>
+      <SearchBar getPokemonByInput={getPokemonByInput}></SearchBar>
     </div>
   );
 }
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SearchBar(props){
+    const [inputValue, setInputValue] = useState();
+    const {getPokemonByInput} = props
+
     return(
         <div className="searchbar_container">
             <div className="input_container">
-                <input type="text"></input>
-                <button>
+                <input type="text" value={inputValue} onInput={(e) => setInputValue(e.target.value)} placeholder="Serach pokemon"></input>
+                <button onClick={() => getPokemonByInput(inputValue)}>
                     <i className="fas fa-search"></i>
                 </button>
             </div>
